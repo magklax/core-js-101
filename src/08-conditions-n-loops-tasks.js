fb9581a5c413d262deeebe661e72d551d95b5596
magklax <nadic2006@yandex.ru>
2020-12-12 15:42:40 +0100

    feat: condition-loop tasks

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -486,10 +486,24 @@ function getMatrixProduct(m1, m2) {
  *    [    ,   ,    ]]
  *
  */
-function evaluateTicTacToePosition(/* position */) {
-  throw new Error('Not implemented');
-}
+function evaluateTicTacToePosition(position) {
+  for (let i = 0; i < position.length; i += 1) {
+    if (position[i][0] && position[i][0] === position[i][1] && position[i][1] === position[i][2]) {
+      return position[i][0];
+    }
 
+    if (position[0][i] && position[0][i] === position[1][i] && position[0][i] === position[2][i]) {
+      return position[0][i];
+    }
+  }
+
+  if (position[1][1] && ((position[0][0] === position[1][1] && position[1][1] === position[2][2])
+  || (position[0][2] === position[1][1] && position[1][1] === position[2][0]))) {
+    return position[1][1];
+  }
+
+  return undefined;
+}
 
 module.exports = {
   getFizzBuzz,
